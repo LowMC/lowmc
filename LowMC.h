2c0397cabd1e6feebaa67d6a688fecc227ecf542
tyti <tyti@dtu.dk>
2016-05-23 15:36:08 +0200

    Changed parameters to match current LowMC version. Added an explanatory comment in test.cpp.

--- LowMC.h
+++ LowMC.h
@@ -8,7 +8,7 @@
 const unsigned numofboxes = 49;    // Number of Sboxes
 const unsigned blocksize = 256;   // Block size in bits
 const unsigned keysize = 80; // Key size in bits
-const unsigned rounds = 11; // Number of rounds
+const unsigned rounds = 12; // Number of rounds
 
 const unsigned identitysize = blocksize - 3*numofboxes;
                   // Size of the identity part in the Sbox layer
--- test.cpp
+++ test.cpp
@@ -8,17 +8,18 @@
 
 int main () {
     // Example usage of the LowMC class
+    // Instantiate a LowMC cipher instance called cipher using the key '1'.
     LowMC cipher(1);
-    block a = 0xFFD5;
+    block m = 0xFFD5;
     
     std::cout << "Plaintext:" << std::endl;
-    std::cout << a << std::endl;
-    a = cipher.encrypt( a );
+    std::cout << m << std::endl;
+    m = cipher.encrypt( m );
     std::cout << "Ciphertext:" << std::endl;
-    std::cout << a << std::endl;
-    a = cipher.decrypt( a );
+    std::cout << m << std::endl;
+    m = cipher.decrypt( m );
     std::cout << "Encryption followed by decryption of plaintext:" << std::endl;
-    std::cout << a << std::endl;
+    std::cout << m << std::endl;
    
     return 0;
 }
